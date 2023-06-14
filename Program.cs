b296bb8f3f544e77353cea9ba876003de38ee98a
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-06-14 14:27:19 -0400

    exercise & challenges complete - adv in prog

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace Roommates
         {
             RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
             RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+            ChoreRepository choreRepo = new ChoreRepository(CONNECTION_STRING);
             bool runProgram = true;
             while (runProgram)
             {
@@ -37,7 +38,10 @@ namespace Roommates
 
                         Room room = roomRepo.GetById(id);
 
-                        Console.WriteLine($"{room.Id} - {room.Name} Max Occupancy({room.MaxOccupancy})");
+                        if (room != null) { Console.WriteLine($"{room.Id} - {room.Name} Max Occupancy({room.MaxOccupancy})"); }
+
+                        else { Console.WriteLine("No room found with that Id."); }
+                           
                         Console.Write("Press any key to continue");
                         Console.ReadKey();
                         break;
@@ -75,7 +79,14 @@ namespace Roommates
 
                         Roommate roommate = roommateRepo.GetById(roommateId);
 
-                        Console.WriteLine($"{roommate.FirstName} {roommate.LastName} pays {roommate.RentPortion}% of the rent and lives in {roommate.Room.Name}.");
+                        if (roommate != null)
+                        {
+                            Console.WriteLine($"{roommate.FirstName} {roommate.LastName} pays {roommate.RentPortion}% of the rent and lives in {roommate.Room.Name}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No roommate found with that Id.");
+                        }
                         Console.Write("Press any key to continue");
                         Console.ReadKey();
                         break;
@@ -117,6 +128,84 @@ namespace Roommates
                         Console.Write("Press any key to continue");
                         Console.ReadKey();
                         break;
+                    case ("Show all chores"):
+                        List<Chore> chores = choreRepo.GetAll();
+                        foreach (Chore c in chores)
+                        {
+                            Console.WriteLine($"{c.Id} - {c.Name}");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Search for chore"):
+                        Console.Write("Chore Id: ");
+                        
+                        int choreId = int.Parse(Console.ReadLine());
+                        
+                        Chore chore = choreRepo.GetById(choreId);
+
+                        if (chore != null) { Console.WriteLine($"{chore.Id} - {chore.Name}"); }
+                        else { Console.WriteLine("No chore found with that Id."); }
+                        
+               
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Add a chore"): 
+                        Console.Write("Chore name: ");
+                        string choreName = Console.ReadLine();
+
+                        Chore choreToAdd = new Chore()
+                        {
+                            Name = choreName,
+                        };
+
+                        choreRepo.Insert(choreToAdd);
+
+                        Console.WriteLine($"{choreToAdd.Name} has been added and assigned an Id of {choreToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Delete a chore"): 
+                        Console.Write("Chore Id: ");
+                        int choreIdToDelete = int.Parse(Console.ReadLine());
+
+                        choreRepo.Delete(choreIdToDelete);
+
+                        Console.WriteLine($"Chore with Id {choreIdToDelete} has been deleted.");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Assign a chore to a roommate"):
+                        // Display all chores
+                        List<Chore> choresList = choreRepo.GetAll();
+                        foreach (Chore c in choresList)
+                        {
+                            Console.WriteLine($"{c.Id} - {c.Name}");
+                        }
+
+                        
+                        Console.Write("Enter the Id of the chore you want to assign: ");
+                        int choreIdToAssign = int.Parse(Console.ReadLine());
+
+                        // Display all roommates
+                        List<Roommate> roommatesList = roommateRepo.GetAll();
+                        foreach (Roommate rm in roommatesList)
+                        {
+                            Console.WriteLine($"{rm.Id} - {rm.FirstName} {rm.LastName}");
+                        }
+
+                        // Prompt user to select a roommate
+                        Console.Write("Enter the Id of the roommate you want to assign the chore to: ");
+                        int roommateIdToAssign = int.Parse(Console.ReadLine());
+
+                        // Assign the chore to the roommate
+                        choreRepo.Assign(choreIdToAssign, roommateIdToAssign);
+
+                        Console.WriteLine("The chore has been successfully assigned.");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;   
                     case ("Exit"):
                         runProgram = false;
                         break;
@@ -135,9 +224,14 @@ namespace Roommates
                 "Search for room",
                 "Add a room",
                 "Show all roommates",
-                "Add a roommate",
                 "Search for roommate",
+                "Add a roommate",
                 "Delete a roommate",
+                "Show all chores",
+                "Search for chore",
+                "Add a chore",
+                "Delete a chore",
+                "Assign a chore to a roommate",
                 "Exit"
             };
 
--- Repositories/ChoreRepository.cs
+++ Repositories/ChoreRepository.cs
@@ -17,5 +17,128 @@ namespace Roommates.Repositories
 
         // ...We'll add some methods shortly...
         // get all, getby id, insert chore, update chore, delete chore
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Chore";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Chore> chores = new List<Chore>();
+
+                    while (reader.Read())
+                    {
+                        var chore = new Chore
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        };
+
+                        chores.Add(chore);
+                    }
+
+                    reader.Close();
+
+                    return chores;
+                }
+            }
+        }
+
+        // GetById Method
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                        @"SELECT Id, Name
+                        FROM Chore
+                        WHERE Id = @id";
+                    
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Chore chore = null;
+
+                    if (reader.Read())
+                    {
+                        chore = new Chore
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        };
+                    }
+
+                    reader.Close();
+                    return chore;
+
+                }
+            }
+        }
+
+        // Insert Method
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Chore (Name)
+                        OUTPUT INSERTED.Id
+                        VALUES (@name)";
+
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+        }
+
+        // Delete Method
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        DELETE FROM Chore
+                        WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Assign Method
+        public void Assign(int choreId, int roommateId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                INSERT INTO RoommateChore (RoommateId, ChoreId)
+                VALUES (@roommateId, @choreId)";
+
+                    cmd.Parameters.AddWithValue("@roommateId", roommateId);
+                    cmd.Parameters.AddWithValue("@choreId", choreId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
